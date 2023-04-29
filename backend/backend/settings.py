14820ee1aee8ddeee779b17536f7f72f02df2e96
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-04-29 23:50:26 +0200

    learn how to set data to template by using django rest framework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "todoapp.apps.TodoappConfig",
+    "frondend.apps.FrondendConfig",
     "rest_framework",
 ]
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("todoapp/", include('todoapp.urls')),
+    path('', include('frondend.urls')),
 ]
